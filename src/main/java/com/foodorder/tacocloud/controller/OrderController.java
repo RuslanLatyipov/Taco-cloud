6e700087f185b5e627cf3ffc9b15652e4954ed6b
Ruslan Latypov <Ruszam1503@mail.ru>
2022-07-31 21:49:35 +0300

    version 1: add configuration properties

--- src/main/java/com/foodorder/tacocloud/controller/OrderController.java
+++ src/main/java/com/foodorder/tacocloud/controller/OrderController.java
@@ -1,12 +1,15 @@
 package com.foodorder.tacocloud.controller;
 
+import com.foodorder.tacocloud.component.OrderProps;
 import com.foodorder.tacocloud.model.TacoOrder;
 import com.foodorder.tacocloud.model.User;
 import com.foodorder.tacocloud.repository.dataJpa.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,15 +26,25 @@ import javax.validation.Valid;
 public class OrderController {
 
     private final OrderRepository orderRepository;
+    private final OrderProps orderProps;
 
     @Autowired
-    public OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository, OrderProps orderProps) {
         this.orderRepository = orderRepository;
+        this.orderProps = orderProps;
     }
 
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @GetMapping
+    public String OrdersForUsers(@AuthenticationPrincipal User user, Model model){
+        Pageable pageable = Pageable.ofSize(orderProps.getPageSize());
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
+
     }
 
     @PostMapping
--- src/main/java/com/foodorder/tacocloud/repository/dataJpa/OrderRepository.java
+++ src/main/java/com/foodorder/tacocloud/repository/dataJpa/OrderRepository.java
@@ -1,10 +1,17 @@
 package com.foodorder.tacocloud.repository.dataJpa;
 
 import com.foodorder.tacocloud.model.TacoOrder;
+import com.foodorder.tacocloud.model.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
     TacoOrder save(TacoOrder tacoOrder);
+
     void deleteAllBy();
+
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 
 }
